e48cc79142f040a6239e34a767bdd570a934718b
Heewon Kim <nabicore@icloud.com>
2017-11-13 10:43:57 +0900

    三角形を出力するソースを組む

--- HelloOpenGL/HelloOpenGL/Tutorial01.cpp
+++ HelloOpenGL/HelloOpenGL/Tutorial01.cpp
@@ -6,7 +6,6 @@
 
 
 int main() {
-    
     // Initialize
     if (!glfwInit())
         return -1;
@@ -28,10 +27,36 @@ int main() {
     if (glewInit() != GLEW_OK)
         return -1;
     
+    GLuint vertexArrayID;
+    glGenVertexArrays(1, &vertexArrayID);
+    glBindVertexArray(vertexArrayID);
+    
+    
     
     // Game Loop
+    
+    // Vertex Data - Triangle
+    static const GLfloat VERTEX_BUFFER_DATA[] = {
+        -1.f, -1.f, 0.f,
+        1.f, -1.f, 0.f,
+        0.f, 1.f, 0.f
+    };
+    
+    // Create vertex buffer
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(VERTEX_BUFFER_DATA), VERTEX_BUFFER_DATA, GL_STATIC_DRAW);
+    
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
     while (true) {
+        // Draw vertex data on screen
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
