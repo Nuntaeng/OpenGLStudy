c87bbf8fe34f6b30ffb94dd51f8c9b27cfc47c0b
R422 <R422@jec.ac.jp>
2017-12-01 12:20:22 +0900

    Add GLM

--- Windows/test/main.cpp
+++ Windows/test/main.cpp
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glew.h>
-#include <GLFW\glfw3.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 
 
 
 int main() {
 
-	if (!glewInit()) {
+	if (!glfwInit()) {
+		fprintf(stderr, "GLFW Initialization Failed!\n");
 		return -1;
 	}
 
@@ -18,19 +20,47 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(1366, 768, "Hello, GL!", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(1366, 768, "Hello, OpenGL!", NULL, NULL);
 	if (!window) {
+		fprintf(stderr, "GLFW Window Generation Failed!\n");
 		glfwTerminate();
 		return -1;
 	}
 
 	glfwMakeContextCurrent(window);
 	glewExperimental = true;
-	if (glewInit() != GLEW_OK)
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to Initialize GLEW\n");
 		return -1;
+	}
 
-	while (true) {
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	static const GLfloat TriangleData[]{
+		-1.f, -1.f, 0.f,
+		1.f, -1.f, 0.f,
+		0.f,  1.f, 0.f
+	};
+
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(TriangleData), TriangleData, GL_STATIC_DRAW);
+
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	do {
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableVertexAttribArray(0);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-	}
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		glfwWindowShouldClose(window) == 0);
 }
